f3cf896e6e2ad72de5c3490ac884e3a25f41a2f5
Kevin Martinez <kkevinmartinez7@gmail.com>
2023-06-08 15:07:17 -0600

    API for projects

--- commands.sh
+++ commands.sh
@@ -10,3 +10,22 @@ pip install djangorestframework
 django-admin startproject drf_simple_crud .
 
 python manage.py startapp projects
+
+# Run App
+python manage.py runserver
+
+#Create migrations
+python manage.py makemigrations
+
+#Excute migrations
+python manage.py migrate
+
+
+
+
+
+#--------------- Renove migrations ---------------
+#find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+#find . -path "*/migrations/*.pyc"  -delete
+#python manage.py makemigrations
+#python manage.py migrate
--- projects/models.py
+++ projects/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    technology = models.CharField(max_length=200)
-    create_at = models.DateTimeField(auto_now_add=True)
+  title = models.CharField(max_length=100)
+  description = models.TextField(blank=True)
+  technology = models.CharField(max_length=20)
+  created_at = models.DateTimeField(auto_now_add=True)
