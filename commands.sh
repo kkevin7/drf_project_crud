08ce033410b6e5c1816c010101be1e1c99db2bba
Kevin Martinez <kkevinmartinez7@gmail.com>
2023-06-08 19:05:34 -0600

    Staring project

---
+++ commands.sh
@@ -0,0 +1,10 @@
+pip install virtualenv
+python -m virtual venv
+
+#windows
+.\venv\Scripts\activate
+
+pip install django
+pip install djangorestframework
+
+django-admin startproject drf_simple_crud .
